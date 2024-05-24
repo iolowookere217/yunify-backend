78006c46d77b85ac4b1baa793b6c72a07da76a7f
godcandidate <godcandidate@gmail.com>
2024-05-24 20:46:23 +0000

    create server.js handles Node.js backend with Firebase

---
+++ server.js
@@ -0,0 +1,22 @@
+// index.js or app.js
+import express from 'express';
+import cors from 'cors';
+//const routes = require('./route/routes');
+
+const app = express();
+
+/* middleware */
+app.use(express.json());
+app.use(cors());
+
+const port = 8080;
+
+//app.use('/', routes); // Mount the routes
+
+try {
+    app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+    });
+} catch (error) {
+    console.log("Cannot connect to the server");
+}
