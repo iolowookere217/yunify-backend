36dffce3f739249cfa197cec12402852121813a1
godcandidate <godcandidate@gmail.com>
2024-05-25 12:39:02 +0000

    update route.js
    Added JWT Auth to update and get user routes

--- route/routes.js
+++ route/routes.js
@@ -2,6 +2,8 @@ import express from 'express';
 import {registerUser, loginUser, getUser, updateUser} from '../controllers/userController.js'
 const router = express.Router();
 
+import Auth from '../middleware/auth.js';
+
 
 router.get('/', (req, res) => {
   res.status(201).json("Home GET Request");
@@ -10,8 +12,9 @@ router.get('/', (req, res) => {
 // User routes
 router.post("/users/register", registerUser);
 router.post("/users/login", loginUser);
-router.get("/users/profile", getUser);
-router.post("/users/update", updateUser);
+router.get("/users/profile", Auth, getUser);
+router.put("/users/update", Auth, updateUser);
+
 
 
 export default router;
