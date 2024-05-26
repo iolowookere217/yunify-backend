b52861a658f37140cf023e598386030f5a732693
godcandidate <godcandidate@gmail.com>
2024-05-26 16:33:53 +0000

    update server.js
    Added bodyParser

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ import express from 'express';
 import cors from 'cors';
 import routes from './route/routes.js';
 
+import bodyParser from 'body-parser';
+
+
+
 const app = express();
 
 /* middleware */
 app.use(express.json());
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true })); // For parsing form data
+app.use(bodyParser.json());
+
 
 const port = 8080;
 
