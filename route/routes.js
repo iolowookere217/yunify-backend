0ed02328d7e7d877565e8410f7b1cf5b8c9b9a51
godcandidate <godcandidate@gmail.com>
2024-05-26 16:32:30 +0000

    update route
    Added route for uploading videos

--- route/routes.js
+++ route/routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import multer from "multer";
 import {registerUser, loginUser, getUser, updateUser} from '../controllers/userController.js'
-const router = express.Router();
-
+import {uploadVideo} from '../controllers/assetController.js';
 import Auth from '../middleware/auth.js';
 
+const router = express.Router();
+
+// handles files upload
+const upload = multer({storage: multer.memoryStorage()});
 
 router.get('/', (req, res) => {
   res.status(201).json("Home GET Request");
@@ -14,7 +18,7 @@ router.post("/users/register", registerUser);
 router.post("/users/login", loginUser);
 router.get("/users/profile", Auth, getUser);
 router.put("/users/update", Auth, updateUser);
-
+router.post("/videos/upload", Auth, upload.single('video'), uploadVideo);
 
 
 export default router;
