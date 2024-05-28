11812e111103ef35f528ceb474c80e95977ee3b7
godcandidate <godcandidate@gmail.com>
2024-05-28 20:38:13 +0000

    update server.js
    - Configured swagger docs
    - Added documentation for user apis and video apis

--- server.js
+++ server.js
@@ -16,8 +16,25 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // For parsing form data
 app.use(bodyParser.json());
 
+// api swagger documentation
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUiExpress from 'swagger-ui-express';
 
 const port = 8080;
+/* swagger */
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "API Endpoints",
+            version: '5.0.0'
+        }
+    },
+    apis: ['server.js'],
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+// route for swagger docs
+routes.use('/docs', swaggerUiExpress.serve,swaggerUiExpress.setup(swaggerDocs));
 
 app.use('/', routes); // Mount the routes
 
@@ -28,3 +45,199 @@ try {
 } catch (error) {
     console.log("Cannot connect to the server");
 }
+
+/* SWAGGER API DOCS*/
+
+/* USERS API */
+// register user
+// user login
+/**
+ * @swagger 
+ * /users/register:
+ *    post:
+ *      tags:
+ *      - User APIs
+ *      summary: Register new user
+ *      description: Registering new user
+ *      parameters:
+ *      - name: User Credentials
+ *        description: Enter user data
+ *        in: body
+ *        type: string
+ *        required: true
+ *        example: {
+ *          "email" : "tdykeff609@tormails.com",
+ *          "password" : "#itworks",
+ *          "name" : "Roronoa Zoro",
+ *          "phoneNumber" : "+23467817181",
+ *          "school" : "Marine Fold",
+ *          "faculty" : "Electronics",
+ *          "department" : "Marine Engineering"
+ *         }
+ *      responses:
+ *        201:
+ *          description: User Registered successfully
+ *        400:
+ *          description: Email already in use 
+ *        500:
+ *          description: User not created
+ * 
+ */
+// user login
+/**
+ * @swagger 
+ * /users/login:
+ *    post:
+ *      tags:
+ *      - User APIs
+ *      summary: Login as user
+ *      description: User logging in
+ *      parameters:
+ *      - name: User Credentials
+ *        description: Enter user data
+ *        in: body
+ *        type: string
+ *        required: true
+ *        example: {
+ *          "email" : "tdykeff609@tormails.com",
+ *          "password" : "#itworks"
+ *         }
+ *      responses:
+ *        201:
+ *          description: Success
+ *        404:
+ *          description: User not found
+ * 
+ */
+//Get user details
+/**
+ * @swagger
+ * /users/profile:
+ *   get:
+ *     tags:
+ *     - User APIs
+ *     summary: Get user  details
+ *     description: User profile 
+ *     security:
+ *     - bearerAuth: []
+ *     parameters:
+ *     - name: Authorization
+ *       in: header
+ *       type: string
+ *       required: true
+ *       description: Bearer token for authentication
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: User not found
+ */
+
+//Update
+/**
+ * @swagger 
+ * /users/update:
+ *    put:
+ *      tags:
+ *      - User APIs
+ *      summary: Modify user data
+ *      description: Changing user data
+ *      security:
+ *       - bearerAuth: []
+ *      parameters:
+ *      - name: Authorization
+ *        in: header
+ *        type: string
+ *        required: true
+ *        description: Bearer token for authentication
+ *      - name: Update user data
+ *        description: Modify or add to data
+ *        in: body
+ *        type: string
+ *        required: false
+ *        example: {
+ *               "faculty": "Navy"
+ *               }
+ *      responses:
+ *        201:
+ *          description: Success
+ *        500:
+ *          description: User update failed
+ * 
+ */
+/* VIDEOS API */
+//Get user details
+/**
+ * @swagger
+ * /videos/metadata:
+ *   get:
+ *     tags:
+ *     - Video APIs
+ *     summary: Get all video details
+ *     description: Video Metadata
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: User not found
+ */
+
+/**
+ * @swagger
+ * /videos/upload:
+ *   post:
+ *     tags:
+ *       - Video APIs
+ *     summary: Upload a video
+ *     description: Upload a video with metadata
+ *     consumes:
+ *       - multipart/form-data
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - name: Authorization
+ *         in: header
+ *         type: string
+ *         required: true
+ *         description: Bearer token for authentication
+ *       - in: formData
+ *         name: video
+ *         type: file
+ *         required: true
+ *         description: The video file to upload
+ *       - in: formData
+ *         name: thumbnail
+ *         type: file
+ *         required: true
+ *         description: The thumbnail image file to upload (optional)
+ *       - in: formData
+ *         name: title
+ *         type: string
+ *         required: true
+ *         description: The title of the video
+ *       - in: formData
+ *         name: course
+ *         type: string
+ *         required: true
+ *         description: Course video addresses
+ *       - in: formData
+ *         name: subject
+ *         type: string
+ *         required: true
+ *         description: Subject video addresses
+ *       - in: formData
+ *         name: description
+ *         type: string
+ *         required: true
+ *         description: Description of the video
+ *       - in: formData
+ *         name: duration
+ *         type: string
+ *         required: true
+ *         description: Duration of the video - hh:mm:ss
+ *     responses:
+ *       201:
+ *         description: Video uploaded successfully
+ *       500:
+ *         description: Internal server error
+ */
